cce6c3979e4ac30d99a459ae9d32da57a67caa72
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2018-04-18 12:15:50 +0430

    start EditorActivity as Launcher

--- app/src/main/java/ir/nadcup/roboro/LaunchActivity.kt
+++ app/src/main/java/ir/nadcup/roboro/LaunchActivity.kt
@@ -2,11 +2,15 @@ package ir.nadcup.roboro
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ir.nadcup.roboro.editor.EditorActivity
 
 class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+
+        EditorActivity.start(this)
+        finish()
     }
 }
