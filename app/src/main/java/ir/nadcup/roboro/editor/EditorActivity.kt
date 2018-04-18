7a9f02ccf39263ade3663780312ea1b4021283fa
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2018-04-18 12:15:12 +0430

    implement Blockly methods

--- app/src/main/java/ir/nadcup/roboro/editor/EditorActivity.kt
+++ app/src/main/java/ir/nadcup/roboro/editor/EditorActivity.kt
@@ -4,30 +4,42 @@ import android.content.Context
 import android.content.Intent
 import com.google.blockly.android.AbstractBlocklyActivity
 import com.google.blockly.android.codegen.CodeGenerationRequest
+import com.google.blockly.android.codegen.LoggingCodeGeneratorCallback
+import com.google.blockly.model.DefaultBlocks
+import java.util.*
+
 
 class EditorActivity : AbstractBlocklyActivity() {
 
     companion object {
+        private val JAVASCRIPT_GENERATORS = Arrays.asList(
+                // Custom block generators go here. Default blocks are already included.
+        ""
+        )
+
         fun start(invokerContext: Context) {
             val startIntent = Intent(invokerContext, EditorActivity::class.java)
             invokerContext.startActivity(startIntent)
         }
     }
 
-    override fun getToolboxContentsXmlPath(): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private var mCodeGeneratorCallback: CodeGenerationRequest.CodeGeneratorCallback = LoggingCodeGeneratorCallback(this, "LoggingTag")
+
+    override fun onInitBlankWorkspace() {
+        // Initialize available variable names.
+        controller.addVariable("item")
     }
+
+    override fun getToolboxContentsXmlPath() = "default/toolbox.xml"
 
     override fun getBlockDefinitionsJsonPaths(): MutableList<String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val assetPaths = ArrayList(DefaultBlocks.getAllBlockDefinitions())
+        // Append your own block definitions here.
+        return assetPaths
     }
 
-    override fun getGeneratorsJsPaths(): MutableList<String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getGeneratorsJsPaths() = JAVASCRIPT_GENERATORS
 
-    override fun getCodeGenerationCallback(): CodeGenerationRequest.CodeGeneratorCallback {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getCodeGenerationCallback() = mCodeGeneratorCallback
 
 }
