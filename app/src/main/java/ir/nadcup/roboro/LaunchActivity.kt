0cb728d903293ad3ee1168816a918ec27cd5e33a
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2018-04-18 10:57:34 +0430

    initial commit

---
+++ app/src/main/java/ir/nadcup/roboro/LaunchActivity.kt
@@ -0,0 +1,12 @@
+package ir.nadcup.roboro
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
